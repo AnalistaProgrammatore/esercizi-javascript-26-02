6d03ca1db78cdd96109919c0c04da846b3be8218
Marcoski <mtrognon@gmail.com>
2020-02-26 14:53:38 +0100

    Update and rename reverse.js to every.js

--- reverse.js
+++
@@ -1,4 +0,0 @@
-/**
-* Scrivere qui il codice dell'esercizio 2 del capitolo 4 di eloquentJS
-* Vi ricordo che le funzioni DEVONO ESSERE PURE e implementate una in modo IMPERATIVO e una in modo DICHIARATIVO
-**/
